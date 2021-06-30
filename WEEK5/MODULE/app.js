20c2bef27ebc99b07e3d7021001013b17f9f9481
martmatics <85228383+martmatics@users.noreply.github.com>
2021-06-30 10:50:14 +0100

    update

---
+++ WEEK5/MODULE/app.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const hostname = '127.0.0.1'
+const port = 1010
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  
+})
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`)
+})

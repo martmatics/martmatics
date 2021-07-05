306b63cc162f93ea6bc7c537b49d879179bc27db
martmatics <85228383+martmatics@users.noreply.github.com>
2021-07-05 09:53:55 +0100

    updates

--- WEEK5/MODULE/app.js
+++ WEEK5/MODULE/app.js
@@ -4,24 +4,34 @@ const http = require('http')
 const hostname = '127.0.0.1'
 const port = 5052
 
-const server = http.createServer((req, res) => {
-  let path = "./views"
-  if (req.url === "/") {
-      path = path + "/index.html"
-  } else if (req.url === "/contact") {
-      path = path + "/contact.html"
-  } else if (req.url === "/about") {
-      path = path + "/about.html"
-  } else if (req.url !== "/contact" || "/about" || "") {
-      path = path + "/error.html"
-  }
+const server = http
+  .createServer((req, res) => {
+    let path = "./";
+    if (req.url === "/" || req.url === "/home") {
+      path = path + "index.html";
+      status = 200;
+    } else if (req.url === "/about" || req.url === "/about-us") {
+      path = path + "about.html";
+      status = 200;
+    } else if (req.url === "/contact") {
+      path = path + "contact.html";
+      status = 200;
+    } else {
+      path = path + "error.html";
+      status = 404;
+    }
 
-  fs.readFile(path, (err, data) => {
-      if (err) return;
-      res.writeHead (200, {"Content-Type": "text/html"});
-      res.write(data);
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        return console.error(err);
+      }
+      res.writeHead(status, { "Content-Type": "text/html" });
+      res.end(data);
+      console.log(`Client requested ${req.url}.`);
+      console.log(`Client was served ${path}.`);
+    });
   })
-})
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`)
 })
